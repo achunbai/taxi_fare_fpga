ecbcbc807e0f236a669f2dc234c2eab52c49dcce
achunbai <47388272+achunbai@users.noreply.github.com>
2022-06-04 15:18:01 +0800

    Revert "Update bcd_adder.v"
    This reverts commit cd0957613e9771a406ce3756a6d720934fa15b99.

--- src/bcd_adder.v
+++ src/bcd_adder.v
@@ -1,11 +1,11 @@
 //一位BCD加法器
 //输入a，b两个加数和前位进位，输出和以及本位进位
 /*
-a	-> 输入的加数a
-b	-> 输入的加数b
+a		-> 输入的加数a
+b		-> 输入的加数b
 c_in	-> 输入的前位进位
 
-sum	-> 输出的和
+sum		-> 输出的和
 c_out	-> 输出的本位进位
 */
 module bcd_adder (
@@ -23,6 +23,6 @@ wire [4:0] ab_sum = {1'b0, a} + {1'b0, b} + {4'b0, c_in};
 assign sum = (ab_sum[3:0] > 4'd9) ? (ab_sum[3:0]-4'd10) : ab_sum[3:0];
 //优先进位计算
 assign c_out = 	(ab_sum[4]) ? 1'b1 : 
-		((ab_sum[3:0] > 4'd9) ? 1'b1 : 1'b0);
+				((ab_sum[3:0] > 4'd9) ? 1'b1 : 1'b0);
 
 endmodule
