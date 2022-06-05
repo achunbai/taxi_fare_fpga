c721c22c05707fb064ce85917dbe46e12f97fcf9
achunbai <47388272+achunbai@users.noreply.github.com>
2022-06-05 18:44:36 +0800

    修复错误
    模块中没有复位信号

--- sim/fare_total_tb.v
+++ sim/fare_total_tb.v
@@ -8,7 +8,6 @@ module fare_total_tb();
 
 reg [15:0] distance_fare_bcd;
 reg [15:0] wait_fare_bcd;
-reg rst_n;
 
 wire [15:0] fare_total_bcd;
 wire max;
@@ -19,9 +18,7 @@ parameter CYCLE = 5'd20;
 initial begin
 	 distance_fare_bcd = 16'b0000_0000_0000_0000;
 	 wait_fare_bcd = 16'b0000_0000_0000_0000;
-	 rst_n = 1'b0;
 	 #(CYCLE * 20);
-	 rst_n = 1'b1;
 	 distance_fare_bcd = 16'b0001_0001_0001_0001;
 	 wait_fare_bcd = 16'b0001_0001_0001_0001;
 	 #(CYCLE * 20);
@@ -43,7 +40,6 @@ end
 fare_total fare_total_test (
 .distance_fare_bcd	(distance_fare_bcd	),
 .wait_fare_bcd		(wait_fare_bcd		),
-.rst_n				(rst_n				),
                    
 .fare_total_bcd		(fare_total_bcd		),
 .max				(max				)
